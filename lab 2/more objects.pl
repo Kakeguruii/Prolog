76f99f9e325661af4297682aaa8340949b33737f
Elizaveta Malezh <milkasps66@gmail.com>
2021-02-28 14:48:49 +0300

    more objects
    Дополнение до 40 объектов в системе

---
+++ lab 2/more objects.pl
@@ -0,0 +1,66 @@
+age(kaeya, 3).
+age(noelle, 2).
+age(aether, 2).
+age(amber, 2).
+age(beidou, 3).
+age(ensan, 1).
+age(chongyun, 2).
+age(xinyan, 2).
+age(xingqiu, 2).
+age(lumine, 2).
+
+gender(kaeya, 1).
+gender(noelle, 2).
+gender(aether, 1).
+gender(amber, 2).
+gender(beidou, 2).
+gender(ensan, 1).
+gender(chongyun, 1).
+gender(xinyan, 2).
+gender(xingqiu, 1).
+gender(lumine, 2).
+
+element(kaeya, 6).
+element(noelle, 4).
+element(aether, 3).
+element(amber, 1).
+element(beidou, 5).
+element(ensan, 1).
+element(chongyun, 6).
+element(xinyan, 1).
+element(xingqiu, 2).
+element(lumine, 3).
+
+nation(kaeya, 1).
+nation(noelle, 1).
+nation(aether, 1).
+nation(amber, 1).
+nation(beidou, 2).
+nation(ensan, 3).
+nation(chongyun, 2).
+nation(xinyan, 2).
+nation(xingqiu, 2).
+nation(lumine, 1).
+
+weapon(kaeya, 3).
+weapon(noelle, 4).
+weapon(aether, 3).
+weapon(amber, 2).
+weapon(beidou, 4).
+weapon(ensan, 4).
+weapon(chongyun, 4).
+weapon(xinyan, 4).
+weapon(xingqiu, 3).
+weapon(lumine, 3).
+
+role(kaeya, 3).
+role(noelle, 1).
+role(aether, 3).
+role(amber, 3).
+role(beidou, 1).
+role(ensan, 3).
+role(chongyun, 3).
+role(xinyan, 1).
+role(xingqiu, 3).
+role(lumine, 3).
+
