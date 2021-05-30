c0edba52570949c20f745b738490c8b46c44df9b
Elizaveta Malezh <milkasps66@gmail.com>
2021-05-30 21:52:15 +0300

    task1

---
+++ lab 9/tasks.pl
@@ -0,0 +1,41 @@
+
+read_list_str(List):-read_str(A,N,Flag),read_list_str([A],List,Flag).
+read_list_str(List,List,1):-!.
+read_list_str(Cur_list,List,0):-
+	read_str(A,N,Flag),append(Cur_list,[A],C_l),read_list_str(C_l,List,Flag).
+
+%1.1
+build_all_razm_p:-
+		read_str(A,_),read(K),b_a_rp(A,K,[]).
+
+b_a_rp(A,0,Perm1):-write_str(Perm1),nl,!,fail.
+b_a_rp(A,N,Perm):-in_list(A,El),N1 is N-1,b_a_rp(A,N1,[El|Perm]).
+
+%1.2
+build_all_perm:-
+		read_str(A,_),b_a_p(A,[]).
+
+in_list_exlude([El|T],El,T).
+in_list_exlude([H|T],El,[H|Tail]):-in_list_exlude(T,El,Tail).
+
+b_a_p([],Perm1):-write_str(Perm1),nl,!,fail.
+b_a_p(A,Perm):-in_list_exlude(A,El,A1),b_a_p(A1,[El|Perm]).
+
+%1.3
+build_all_razm:-
+		read_str(A,N),read(K),b_a_r(A,K,[]).
+b_a_r(A,0,Perm1):-write_str(Perm1),nl,!,fail.
+b_a_r(A,N,Perm):-in_list_exlude(A,El, A1),N1 is N-1,b_a_r(A1,N1,[El|Perm]).
+
+%1.4
+sub_set([],[]).
+sub_set([H|Sub_set],[H|Set]):-sub_set(Sub_set,Set).
+sub_set(Sub_set,[H|Set]):-sub_set(Sub_set,Set).
+pr_subset:-read_str(A,N),sub_set(B,A),write_str(B),nl,fail.
+
+%1.5
+sochet([],0,_):-!.
+sochet([H|Sub_set],K,[H|Set]):-K1 is K-1,sochet(Sub_set,K1,Set).
+sochet(Sub_set,K,[_|Set]):-sochet(Sub_set,K,Set).
+pr_sochet:-read_str(A,N),read(K),sochet(B,K,A),write_str(B),nl,fail.
+
